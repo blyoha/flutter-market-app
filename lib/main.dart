90af7ea275ddd4a30b178e7f0c67931609093c80
blyoha <filling_box@yahoo.com>
2022-08-05 00:53:06 +0300

    Changed style.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ozon_app/screens/home_screen.dart';
+import 'package:ozon_app/utils/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,21 +13,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: AppColors.primaryColor));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ozon Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.indigo,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          textTheme: TextTheme(
+              bodyText2:
+                  TextStyle(color: AppColors.primaryColor, fontSize: 15)),
+          primarySwatch: Colors.indigo,
+          backgroundColor: AppColors.backgroundColor,
+          fontFamily: 'Lato'),
       home: const HomeScreen(),
     );
   }
