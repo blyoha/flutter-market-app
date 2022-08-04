34fcad0eeb5bab1ca30d51640566c6869383fa67
blyoha <filling_box@yahoo.com>
2022-08-05 00:52:47 +0300

    Added offers section.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ozon_app/utils/app_colors.dart';
 import 'package:ozon_app/widgets/search_bar.dart';
-import 'package:ozon_app/widgets/section.dart';
+import 'package:ozon_app/widgets/section_item.dart';
+import 'package:ozon_app/widgets/store_item.dart';
+import 'package:ozon_app/widgets/text.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,91 +13,119 @@ class HomeScreen extends StatelessWidget {
     String pickupPoint = "ул. Бабушкина, 223";
 
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: AppColors.backgroundColor,
       body: SafeArea(
         child: Stack(
           children: [
             // top search
             Positioned(
+                top: 0,
+                left: 0,
                 width: MediaQuery.of(context).size.width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    // flight icon
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(Icons.flight),
-                    ),
-                    // search bar
-                    Expanded(
-                      child: SearchBar(),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(Icons.notifications_none),
-                    )
-                  ],
+                child: Container(
+                  padding: const EdgeInsets.all(5.0),
+                  color: AppColors.primaryColor,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      // flight icon
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(Icons.flight, color: Colors.white),
+                      ),
+                      // search bar
+                      Expanded(
+                        child: SearchBar(),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(Icons.notifications_none, color: Colors.white),
+                      )
+                    ],
+                  ),
                 )),
             // main content
-            Positioned(
-                top: 50,
-                left: 0,
+            Container(
+                padding: const EdgeInsets.only(top: 50),
                 height: double.maxFinite,
                 width: MediaQuery.of(context).size.width,
                 child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
                     child: Column(
-                  children: [
-                    // pickup point
-                    Container(
-                      decoration: const BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(color: Colors.red, width: 1))),
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        children: [
-                          const Icon(Icons.location_on_outlined),
-                          Text("Пункт Ozon | $pickupPoint"),
-                          Expanded(child: Container()),
-                          const Icon(Icons.arrow_forward_ios),
-                        ],
-                      ),
-                    ),
-                    // recommendation cards
-                    SizedBox(
-                      height: 180,
-                      child: PageView(
-                          children: List.generate(
-                              3,
-                              (index) => Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: [Colors.indigo, Colors.orange, Colors.purple][index],
-                                    ),
-                                    margin: const EdgeInsets.all(20),
-                                    child: Center(
-                                        child: Text("RECOMMENDATION ${index+1}")),
-                                  ))),
-                    ),
-                    // sections
-                    SizedBox(
-                      height: 180,
-                      child: PageView(children: [
-                        Wrap(
-                            children: List.generate(
-                                6,
-                                (index) => const Section(
-                                    icon: Icons.menu_outlined,
-                                    text: "Каталог"))),
-                        Wrap(
-                            children: List.generate(
-                                6,
-                                (index) => const Section(
-                                    icon: Icons.document_scanner_outlined,
-                                    text: "Офис"))),
-                      ]),
-                    )
-                  ],
-                ))),
+                      children: [
+                        // pickup point
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                      color: AppColors.secondaryColor,
+                                      width: 1))),
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Icon(Icons.location_on_outlined, color: AppColors.primaryColor),
+                              SimpleText(text: "Пункт Ozon | $pickupPoint", color: AppColors.primaryColor),
+                              Expanded(child: Container()),
+                              Icon(Icons.arrow_forward_ios, color: AppColors.primaryColor),
+                            ],
+                          ),
+                        ),
+                        // recommendation cards
+                        SizedBox(
+                          height: 180,
+                          child: PageView(
+                              children: List.generate(
+                                  3,
+                                  (index) => Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          color: [
+                                            Colors.indigo,
+                                            Colors.orange,
+                                            Colors.purple
+                                          ][index],
+                                        ),
+                                        margin: const EdgeInsets.all(20),
+                                        child: Center(
+                                            child: Text(
+                                                "RECOMMENDATION ${index + 1}")),
+                                      ))),
+                        ),
+                        // sections
+                        SizedBox(
+                          height: 180,
+                          child: PageView(children: [
+                            Wrap(
+                                children: List.generate(
+                                    6,
+                                    (index) => const SectionItem(
+                                        icon: Icons.menu_outlined,
+                                        text: "Каталог"))),
+                            Wrap(
+                                children: List.generate(
+                                    6,
+                                    (index) => const SectionItem(
+                                        icon: Icons.document_scanner_outlined,
+                                        text: "Офис"))),
+                          ]),
+                        ),
+                        // offers
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Wrap(
+                            alignment: WrapAlignment.center,
+                            children: List.generate(6, (index) {
+                              return const StoreItem(
+                                  "Рюкзак Mr. Skinner Летнее настроение",
+                                  1475,
+                                  5,
+                                  "assets/images/backpack.webp");
+                            }),
+                          ),
+                        )
+                      ],
+                    ))),
           ],
         ),
       ),
