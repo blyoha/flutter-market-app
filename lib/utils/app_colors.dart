49c58ca29e1b713070fc58d8332b5abfac4a6f0b
blyoha <filling_box@yahoo.com>
2022-08-05 00:11:27 +0300

    Added colors.

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = const Color.fromRGBO(25, 33, 38, 1);
+  static Color secondaryColor = const Color.fromRGBO(25, 33, 38, 0.7);
+
+  static Color focusColor = const Color.fromRGBO(187, 242, 70, 1);
+  static Color backgroundColor = const Color.fromRGBO(247, 246, 250, 1);
+  static Color backgroundColor2 = const Color.fromRGBO(241, 241, 241, 1);
+
+  static Color searchBarColor = const Color.fromRGBO(140, 144, 147, 1);
+}
