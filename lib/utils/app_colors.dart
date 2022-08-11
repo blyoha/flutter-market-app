fc24008bea422fd8de19403cec587d75db083f04
blyoha <filling_box@yahoo.com>
2022-08-11 17:26:53 +0300

    Changed colors.

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color primaryColor = const Color.fromRGBO(25, 33, 38, 1);
-  static Color secondaryColor = const Color.fromRGBO(25, 33, 38, 0.7);
+  static Color secondaryColor = const Color.fromRGBO(112, 127, 141, 1);
+  static Color backgroundColor = const Color.fromRGBO(242, 245, 249, 1);
 
-  static Color focusColor = const Color.fromRGBO(187, 242, 70, 1);
-  static Color backgroundColor = const Color.fromRGBO(247, 246, 250, 1);
-  static Color backgroundColor2 = const Color.fromRGBO(241, 241, 241, 1);
+  static Color focusColor = const Color.fromRGBO(0, 91, 255, 1);
+  static Color focusColor2 = const Color.fromRGBO(249, 17, 85, 1);
+  static Color priceColor = const Color.fromRGBO(16, 196, 76, 1);
+  static Color starColor = const Color.fromRGBO(255, 184, 0, 1);
 
   static Color searchBarColor = const Color.fromRGBO(140, 144, 147, 1);
 }
