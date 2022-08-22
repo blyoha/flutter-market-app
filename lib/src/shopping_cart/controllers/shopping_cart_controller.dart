dd8d5e57deff0675fb8a63ddaa5c4fd754ef8a0c
blyoha <filling_box@yahoo.com>
2022-08-23 02:07:03 +0300

    Added notification when add/remove a store item. Added getters.

--- lib/src/shopping_cart/controllers/shopping_cart_controller.dart
+++ lib/src/shopping_cart/controllers/shopping_cart_controller.dart
@@ -1,20 +1,39 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../shopping_cart/models/store_item_model.dart';
 
 class ShoppingCartController extends GetxController {
   // Dictionary to store the store items
-  var storeItems = {}.obs;
+  final _storeItems = {}.obs;
 
   // Logic of adding a store item to the cart
   void addStoreItem(StoreItemModel storeItem) {
-    if (storeItems.containsKey(storeItem)) {
-      storeItems[storeItem]++;
+    if (_storeItems.containsKey(storeItem)) {
+      _storeItems[storeItem]++;
     } else {
-      storeItems[storeItem] = 1;
+      _storeItems[storeItem] = 1;
     }
+
+    Get.snackbar("Added to cart", "You've added the ${storeItem.name}",
+        snackPosition: SnackPosition.BOTTOM,
+        duration: const Duration(seconds: 1),
+        margin: const EdgeInsets.only(bottom: 30));
   }
 
   // Logic of removing a store item from the cart
-  void removeStoreItem(StoreItemModel storeItem) {}
+  void removeStoreItem(StoreItemModel storeItem) {
+    Get.snackbar("Removed from cart", "You've removed the ${storeItem.name}",
+        snackPosition: SnackPosition.BOTTOM,
+        duration: const Duration(seconds: 1),
+        margin: const EdgeInsets.only(bottom: 30));
+  }
+
+  // Getting all items in the cart.
+  get storeItems => _storeItems;
+
+  // Get item quantity.
+  int getQuantity(StoreItemModel storeItem) {
+    return _storeItems[storeItem];
+  }
 }
