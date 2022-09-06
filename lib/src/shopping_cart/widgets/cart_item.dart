01b512155c179a86926cf303be85cb01f0251fc0
blyoha <filling_box@yahoo.com>
2022-09-06 18:13:58 +0300

    Fixed installment.

--- lib/src/shopping_cart/widgets/cart_item.dart
+++ lib/src/shopping_cart/widgets/cart_item.dart
@@ -30,18 +30,22 @@ class CartItem extends StatefulWidget {
 class _CartItemState extends State<CartItem> {
   @override
   Widget build(BuildContext context) {
+    final int installment = (widget.storeItem.price *
+            widget.controller.storeItems[widget.storeItem] /
+            6)
+        .ceil();
+
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 15),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // checkbox + image
-              Row(
-                children: [
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // checkbox + image
+                Row(children: [
                   Checkbox(
                       side: BorderSide(
                           color: AppColors.primaryColor.withOpacity(0.1),
@@ -62,58 +66,48 @@ class _CartItemState extends State<CartItem> {
                     return Image.asset("assets/images/network_error.png",
                         height: 80, width: 80, fit: BoxFit.fitHeight);
                   })
-                ],
-              ),
-              // item info
-              Container(
-                margin: const EdgeInsets.only(left: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // price
-                    Header(
-                      text:
-                          "${widget.controller.subTotalPrice[widget.index]} руб",
-                      color: AppColors.primaryColor,
-                      size: 15,
-                    ),
-                    const SizedBox(height: 5),
-                    // name
-                    SizedBox(
-                      width: 210,
-                      child: Flexible(
-                        child: Text(
-                          widget.storeItem.name,
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+                ]),
+                // item info
+                Container(
+                    margin: const EdgeInsets.only(left: 20),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // price
+                          Header(
+                            text:
+                                "${widget.controller.subTotalPrice[widget.index]} руб",
+                            color: AppColors.primaryColor,
+                            size: 15,
+                          ),
+                          const SizedBox(height: 5),
+                          // name
+                          SizedBox(
+                              width: 210,
+                              child: Flexible(
+                                  child: Text(
+                                widget.storeItem.name,
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                              )))
+                        ]))
+              ]),
           const SizedBox(height: 20),
           // installment
           FittedBox(
-            child: Container(
-              margin: const EdgeInsets.only(left: 15),
-              height: 20,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: const Color.fromRGBO(254, 204, 50, 1)),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5),
-                child: SimpleText(
-                    text:
-                        "Частями по ${(widget.storeItem.price / 6).ceil()} / мес",
-                    color: AppColors.primaryColor,
-                    size: 12),
-              ),
-            ),
-          ),
+              child: Container(
+                  margin: const EdgeInsets.only(left: 15),
+                  height: 20,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromRGBO(254, 204, 50, 1)),
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: SimpleText(
+                          text: "Частями по $installment / мес",
+                          color: AppColors.primaryColor,
+                          size: 12)))),
           Divider(
               thickness: 1, color: AppColors.secondaryColor.withOpacity(0.3)),
           // buttons bar
@@ -161,13 +155,9 @@ class _CartItemState extends State<CartItem> {
                       padding: const EdgeInsets.only(left: 10),
                       child: Row(children: [
                         Padding(
-                          padding: const EdgeInsets.only(right: 5),
-                          child: Icon(
-                            Icons.delete_outline_rounded,
-                            color: AppColors.primaryColor,
-                            size: 20,
-                          ),
-                        ),
+                            padding: const EdgeInsets.only(right: 5),
+                            child: Icon(Icons.delete_outline_rounded,
+                                color: AppColors.primaryColor, size: 20)),
                         const Text("Удалить")
                         // SimpleText(text: "Удалить", color: AppColors.primaryColor)
                       ]),
@@ -187,12 +177,10 @@ class _CartItemState extends State<CartItem> {
                   child: Row(
                     children: [
                       Text("${widget.quantity} шт."),
-                      Column(
-                        children: const [
-                          Icon(Icons.keyboard_arrow_up_rounded, size: 20),
-                          Icon(Icons.keyboard_arrow_down_rounded, size: 20)
-                        ],
-                      )
+                      Column(children: const [
+                        Icon(Icons.keyboard_arrow_up_rounded, size: 20),
+                        Icon(Icons.keyboard_arrow_down_rounded, size: 20)
+                      ])
                     ],
                   ),
                 )
