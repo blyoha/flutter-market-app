ffb4b19070065337446a233db44d39267ebecaf2
blyoha <filling_box@yahoo.com>
2022-09-06 17:11:00 +0300

    Fixed initially active "Check all" checkbox bug.

--- lib/src/shopping_cart/controllers/cart_controller.dart
+++ lib/src/shopping_cart/controllers/cart_controller.dart
@@ -53,7 +53,9 @@ class CartController extends GetxController {
     return _storeItems.values.reduce((value, element) => value + element);
   }
 
-  get allChecked => _storeItems.keys.toList().every((item) => item.isChecked);
+  get allChecked => _storeItems.isEmpty
+      ? false
+      : _storeItems.keys.toList().every((item) => item.isChecked);
 
   void checkAll(bool value) {
     _storeItems.forEach((k, v) {
